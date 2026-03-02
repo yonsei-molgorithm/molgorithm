59f1cf57bd325e52b79d0814f6b24d75efc90d8f
pggggggggh <pggggggggh@yonsei.ac.kr>
2026-03-02 16:12:47 +0900

    feat: sections

--- components/main/about-section.tsx
+++ components/main/about-section.tsx
@@ -1,12 +1,24 @@
 export function AboutSection() {
   return (
-    <section className="mx-auto w-full max-w-5xl px-4 py-20">
-      <div className="rounded-2xl border bg-card p-8">
-        <h2 className="text-2xl font-semibold">About Molgorithm</h2>
-        <p className="mt-4 text-muted-foreground">
-          ㅁㄴㅇㄻㄴㅇㄻ
-        </p>
+    <section className="mx-auto w-full max-w-5xl px-4">
+      <div className="p-8">
+        <h2 className="text-2xl font-bold text-primary">소개</h2>
+        <p className="mt-4 tracking-tight">
+          우리는 생각하고, 문제를 풀고, 경쟁합니다. 모르고리즘은 단순히 코드를 작성하는 곳이 아닙니다. 코드는 여러분의 풀이를 구현하는 매개체일 뿐, 우리는 '문제 해결'에 집중하며, 동료들과 매주 대회에 참여하고 솔루션을 공유하며 성장하고 있습니다. </p>
+      </div>
+            <div className="p-8">
+        <h2 className="text-2xl font-bold text-primary">자랑</h2>
+        <p className="mt-4 tracking-tight">
+          안원영과 카리나를 소유하고 있습니다.</p>
+      </div>
+
+                  <div className="p-8">
+        <h2 className="text-2xl font-bold text-primary">Alumni Day</h2>
+        <p className="mt-4 tracking-tight">
+        모르고리즘에 참여했던 선배님들과 학회원들이 한자리에 모여 선후배 간의 네트워킹을 도모하는 행사입니다. 이 자리에서는 선배님들께 알고리즘 학습, 학교 생활, 보안, 금융 분야에 관한 실제 경험담을 들을 수 있으며 이로 인해 학회원들은 선배님들의 경험과 더불어 더 넓은 시야와 정보를 가져갈 수 있습니다.
+          </p>
       </div>
     </section>
+    
   );
 }
